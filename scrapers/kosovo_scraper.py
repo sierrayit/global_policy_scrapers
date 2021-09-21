54df3568579c1811c7604c92e46981f552b0d6d8
Sierra Yit <sierrayit@gmail.com>
2021-09-21 08:40:04 -0400

    style fixes for Kosovo scraper.

--- scrapers/kosovo_scraper.py
+++ scrapers/kosovo_scraper.py
@@ -1,43 +1,44 @@
-import re
-import csv
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
+"""Download all laws from the Kosovo website."""
+from datetime import date
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-base_url = 'https://gzk.rks-gov.net'
-law_list = 'https://gzk.rks-gov.net/LawInForceList.aspx'
+BASE_URL = 'https://gzk.rks-gov.net'
+START_URL = 'https://gzk.rks-gov.net/LawInForceList.aspx'
 WEB_DRIVER_PATH = '/usr/local/bin/chromedriver'
 
-links = []
-metadata = []
-metadata_path = 'scraper_metadata.csv'
+LINKS = []
+METADATA = []
+METADATA_PATH = '../data/kosovo/metadata.csv'
+DOWNLOAD_PATH = '../data/kosovo/txt'
 
-def GetLinksAndNext(atags):
+def get_links_and_next(atags):
+    """Populates the list of LINKS to follow."""
     for atag in atags:
         link = atag.get_attribute('href')
-        if link == None:
+        if link is None:
             continue
         if 'https' in link:
-            links.append(link)
+            LINKS.append(link)
         class_attr = atag.get_attribute('class')
-        id = atag.get_attribute('id')
-        if class_attr != None and class_attr == 'Linkbutton' and 'Next' in id:
+        tag_id = atag.get_attribute('id')
+        if class_attr is not None and class_attr == 'Linkbutton' and 'Next' in tag_id:
             return atag
     return None
 
-def GetLawText(driver, law_link):
+def get_law_text(driver, law_link):
+    """Get the text of a law."""
     print('Getting text for link: ' + law_link)
     driver.get(law_link)
 
     # Click button for english
-    driver.find_elements_by_xpath('/html/body/form/div[3]/div[1]/div[1]/div[2]/div[2]/ul/li[2]/a')[0].click()
+    driver.find_elements_by_xpath(
+        '/html/body/form/div[3]/div[1]/div[1]/div[2]/div[2]/ul/li[2]/a')[0].click()
 
-    # Collect metadata
-    type_of_act = driver.find_elements_by_xpath('/html/body/form/div[3]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[1]/table/tbody/tr[1]/td[3]/span')[0].text
-    act_number = driver.find_elements_by_xpath('/html/body/form/div[3]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[1]/table/tbody/tr[2]/td[3]/span')[0].text
-    main_law_page_button = driver.find_elements_by_xpath('/html/body/form/div[3]/div[1]/div[2]/div[2]/div[2]/div/div[1]/div/div/div[1]/a')
+    main_law_page_button = driver.find_elements_by_xpath(
+        '/html/body/form/div[3]/div[1]/div[2]/div[2]/div[2]/div/div[1]/div/div/div[1]/a')
 
     main_law_title = main_law_page_button[0].text
     is_abolished = driver.find_elements_by_xpath('//*[@id="MainContent_lblAct_Ne_Fuqi_Txt"]')
@@ -50,51 +51,49 @@ def GetLawText(driver, law_link):
     law_text = driver.find_elements_by_xpath('//*[@id="MainContent_txtDocument"]')[0].text
 
     title = main_law_title.strip().replace(' ', '-').replace('/','-')[:250]
-    filename = 'txt/' + title + '.txt'
-    metadata.append({'title': main_law_title, 'link': law_link, 'download_path': filename})
-    file = open(filename,"a")
-    file.write(law_text)
-    file.close()
+    filename = DOWNLOAD_PATH + title + '.txt'
+    METADATA.append({'title': main_law_title,
+                     'country': 'Kosovo',
+                     'link': law_link,
+                     'date_scraped': date.today().strftime('%Y-%m-%d'),
+                     'download_path': filename,})
+    with open(filename, "a") as file_handle:
+        file_handle.write(law_text)
+        file_handle.close()
 
-def WriteMetadataCsv():
-    print('Writing scraper metadata csv')
-    with open(metadata_path, 'w', newline='') as csvfile:
-        fields = ['title', 'country', 'link', 'date_scraped', 'download_path']
-        writer = csv.DictWriter(csvfile, fieldnames=fields)
-        writer.writeheader()
-        for d in metadata:
-            writer.writerow({'title': d['title'],
-                            'country': 'Kosovo',
-                            'link': d['link'],
-                            'date_scraped': '2021/03/30',
-                            'download_path': d['download_path']})
+def write_metadata_json():
+    """Write the metadata file."""
+    print('Writing metadata to json')
+    with open(METADATA_PATH, 'w') as file:
+        json.dump(METADATA, file)
 
-def main():
+def scrape_kosovo_laws():
+    """Scrapes all laws from the Kosovo site."""
     options = Options()
     options.headless = True
     options.add_argument("--window-size=1920,1200")
 
     driver = webdriver.Chrome(options=options, executable_path=WEB_DRIVER_PATH)
-    driver.get(law_list)
+    driver.get(START_URL)
 
     atags = driver.find_elements_by_tag_name('a')
-    res = GetLinksAndNext(atags)
-    while (res != None):
+    res = get_links_and_next(atags)
+    while res is not None:
         res.click()
         atags = driver.find_elements_by_tag_name('a')
-        res = GetLinksAndNext(atags)
+        res = get_links_and_next(atags)
 
-    print('Finished getting links. Found ' + str(len(links)))
+    print('Finished getting links. Found ' + str(len(LINKS)))
 
     done = 1
-    for link in links:
-        print('Processing link: ' + str(done) + '/' + str(len(links)))
-        GetLawText(driver, link)
+    for link in LINKS:
+        print('Processing link: ' + str(done) + '/' + str(len(LINKS)))
+        get_law_text(driver, link)
         done += 1
 
-    WriteMetadataCsv()
+    write_metadata_json()
     driver.quit()
 
 
 if __name__ == '__main__':
-  main()
+    scrape_kosovo_laws()
