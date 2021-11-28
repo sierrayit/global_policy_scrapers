add4ff9c1d126b95c9039df0e1deb8e1545779dc
tm <tzemin.koay@gmail.com>
2021-11-28 22:25:58 +0800

    fixed bugs and added documentation for Vietnam scraper

--- scrapers/vietnam_scraper.py
+++ scrapers/vietnam_scraper.py
@@ -1,5 +1,17 @@
 """
 Scraper for Vietnam's laws found in http://vbpl.vn/TW/Pages/vbpqen.aspx. 
+
+Step 1: Gather base links.
+Collect a set of base urls, where each url corresponds to a type of document. Each document is associated with an index, i:
+f"https://vbpl.vn/TW/Pages/vanbanTA.aspx?idLoaiVanBan={i}".
+
+Step 2: Access each base link, which may contain multiple pages, and scrape metadata and law document(s) page by page.
+- Metadata: [title, link, download_date, country, date_enacted, date_effective, document_type, status, description, download_path, language]
+  Not all will exist, esp. for date_enacted and date_effective.
+- Law document(s): Look for and download all valid file attachments first. If absent, scrape the text and save as txt.
+  Download English content first, then if Vietnamese docs are present, download those too. Metadata for Vietnamese docs is in English, only the docs
+  are in Vietnamese.
+
 """
 
 from datetime import date, datetime
@@ -9,6 +21,7 @@ import re
 import json
 import os
 from bs4 import BeautifulSoup
+
 
 HOME_DIR = os.path.dirname(os.path.dirname(__file__))
 DOWNLOAD_PATH = os.path.join(HOME_DIR, "data", "vietnam")
@@ -21,7 +34,7 @@ METADATA = []
 
 
 def gather_baselinks(max_index = 24):
-    """Gather link per type of document. Each max_index value corresponds to a document type."""
+    """Gather link per type of document. Each max_index value corresponds to a document type. 24 seems to be the max."""
 
     print("gathering baselinks")
     
@@ -46,7 +59,7 @@ def loop_through_paging():
     """Navigate page by page based on each url in BASE_URLS and scrape information per page."""
 
     for base_url, doctype in BASE_URLS:
-        print("===", doctype, ":", base_url, "===")
+        print("==========", doctype, base_url, "==========")
         table_exists = True
         i = 1
 
@@ -89,8 +102,8 @@ def scrape_documents_info(soup, doctype):
         url = BASE_URL + titles[i]["href"]
         title = titles[i].get_text()
         description = descs[i].get_text()
-        published_date = validated_date(pubdates[i]) #datetime.strptime(pubdates[i], "%d/%m/%Y").strftime("%Y-%m-%d") 
-        effective_date = validated_date(effdates[i]) #datetime.strptime(effdates[i], "%d/%m/%Y").strftime("%Y-%m-%d")
+        published_date = validated_date(pubdates[i])
+        effective_date = validated_date(effdates[i])
 
         # enter document url, gather additional info and append to metadata - only for English
         language = "english"
@@ -149,12 +162,19 @@ def validated_date(date_string):
 def find_download_links(soup, title, language):
     """Examine all download links per law document and create respective filepaths."""
 
-    # check if file attachment elements exist
     vbfile = soup.find("div", "vbFile")
-    
+    fulltext = soup.find("div", "fulltext")
+
+    # check if file attachment elements exist
     if vbfile is not None:
         attach = vbfile.select("ul li a")
         metadata_list = [] # collect metadata for link and download_path
+
+        # some laws have multiple doc links, so we want to alter the saved doc's filename to prevent overwriting
+        multiple = len(attach) > 1
+        if multiple:
+            title += "__"
+            i = 1
 
         # loop through every available file attachment
         for a in attach:
@@ -169,6 +189,11 @@ def find_download_links(soup, title, language):
             doc = requests.get(url)
             ext = re.split("\.", fpath)[-1]
 
+            # some laws have multiple doc links, so we alter the saved doc's filename to prevent overwriting
+            if multiple:
+                title = title[:-1] + str(i)
+                i += 1
+
             fname = create_filename(title, language, ext)
             with open(fname, "wb") as f:
                 for chunk in doc.iter_content(1024 * 1024):
@@ -180,14 +205,18 @@ def find_download_links(soup, title, language):
         return metadata_list
 
     # if file attachment elements don't exist, scrape the text off the page and save as txt
-    else:
-        doc = soup.find("div", class_ = "fulltext").get_text()
+    elif fulltext is not None:
+        doc = fulltext.get_text()
         fname = create_filename(title, language, "txt")
         with open(fname, "w", encoding = "utf-8") as f:
             f.write(doc)
 
         print("downloaded txt for", title)
         return [{"download_path": fname, "language": language}] # alternative for "download_path": [fname.index("data"):]
+
+    # if neither exists, don't save law document
+    else:
+        return None
 
 
 def create_filename(title, language, ext):
@@ -201,6 +230,9 @@ def create_filename(title, language, ext):
 
 def append_metadata(mdict, metadata_list):
     """Add other key-value pairs to the metadata dictionary, mdict, and append mdict to METADATA."""
+
+    if metadata_list is None:
+        return
     
     for m in metadata_list:
         mdict["download_path"] = m["download_path"]
@@ -223,8 +255,8 @@ def write_metadata_json():
 
 
 def scrape_vietnam_laws():
-    gather_baselinks()
-    loop_through_paging()
+    gather_baselinks()         # run gather_baselinks(1) for quick sample of results
+    loop_through_paging()        
     write_metadata_json()
 
 if __name__ == '__main__':
